cecad1810b118d4e7c9018b8c5e11db81d4b16a6
Ajay Kumar <mr.ajaysingh8809@gmail.com>
2025-09-16 23:52:03 +0530

    Update ReverseVowelsOfAString.cpp

--- Array & String/ReverseVowelsOfAString.cpp
+++ Array & String/ReverseVowelsOfAString.cpp
@@ -3,15 +3,15 @@ public:
     bool isVowel(char c){
        return ( c == 'a' || c == 'e' || c == 'i' || c == 'o' ||
                    c == 'u' || c== 'A' || c== 'E' || c == 'I' ||
-                   c =='O' || c == 'U');
+                   c =='O' || c == 'U');  // for vowel check
     }
     string reverseVowels(string s) {
         int n = s.size();
         int i = 0, j = n - 1;
         while (i < j) {
-            while(i<j && !isVowel(s[i])) i++;
+            while(i<j && !isVowel(s[i])) i++;  // vowel checking
             while(i<j && !isVowel(s[j])) j--;
-            swap(s[i], s[j]);
+            swap(s[i], s[j]); // swaping vowels
             i++, j--;
         }
         return s;
