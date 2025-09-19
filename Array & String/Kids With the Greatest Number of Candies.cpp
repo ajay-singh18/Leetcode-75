ca5bae6f22896ff67ee9d6ddebd64cd18f5cbd98
Ajay Kumar <mr.ajaysingh8809@gmail.com>
2025-09-20 00:00:16 +0530

    Update Kids With the Greatest Number of Candies.cpp

--- Array & String/Kids With the Greatest Number of Candies.cpp
+++ Array & String/Kids With the Greatest Number of Candies.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         int n = candies.size();
-        vector<bool> ans(n);
+        vector<bool> ans(n); //
         int maxElement = *max_element(candies.begin(),candies.end());
         for(int i = 0 ;i<n;i++){
             ans[i] = (candies[i]+extraCandies >= maxElement);
